ff4c2e8d45cdc9ce86ec57457288593eceb492a5
unknown <ggdzqy@qq.com>
2024-04-17 13:46:46 +0800

    fix2

--- functions/[[date]].js
+++ functions/[[date]].js
@@ -1,4 +1,3 @@
-const newHomepagePathName = "/test"
 
 export async function onRequest(context) {
   // Contents of context object
@@ -12,20 +11,6 @@ export async function onRequest(context) {
   } = context;
   //get the request url
   const url = new URL(request.url);
-  if (url.pathname === "/") {
-    return context.next();
-  }else{
-    if (context.params.date) {
-      console.log(JSON.stringify(context.params.date));
-      //return new Response(JSON.stringify(context.params.date));
-      //return Response.redirect(url.origin+"/_index.html", 302)
-      // pass the request to /test
-      url.pathname = newHomepagePathName
-      return new Response("/test", { status: 301 });
-    }
-    else{
-      //redirect to index page
-      return Response.redirect(url.origin+"/_index.html", 302)
-    }
-  }
+  console.log(JSON.stringify(context.params.date));
+  return context.next();
 }
